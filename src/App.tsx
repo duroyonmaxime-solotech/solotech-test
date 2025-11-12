8a68a27e5b0e2f1bcbc5138dc40d8e12d5a1a1e7
Maxime Duroyon <maxime.duroyon@solotech.com>
2025-11-11 19:01:39 -0500

    add errors

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,21 @@ import Btn from './Button.tsx'
 export default function App() {
   const [count, setCount] = React.useState(0)
   const [mounted, setMounted] = React.useState(true)
+  const [pokemons, setPokemons] = React.useState([])
   let displayCard = React.useRef(false)
 
   React.useEffect(() => {
+    getData()
     if (mounted) {
       console.log('App mounted')
     }
     setMounted(true)
 
-    // React.useEffect(() => {
-    //   return () => {
-    //     console.log('App unmounted')
-    //   }
-    // }, [])
+    React.useEffect(() => {
+      return () => {
+        console.log('App unmounted')
+      }
+    }, [])
 
   }, [count, mounted])
 
@@ -24,22 +26,36 @@ export default function App() {
     setCount((count as unknown as number) + 1)
   }
 
-  const Cardd = (title, description, footer) => {
+  async function getData() {
+    try {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0");
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      setPokemons(result.results);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  const Card = (title, description, level) => {
     return (
-        <div>
-            <h2>{title}</h2>
-            <p>{description}</p>
-        </div>
+      <div>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
     )
   }
 
   return (
     <div>
-        <h1>Mini React app</h1>
-        <Btn label="Click me" onClick={() => handleClick()} /> 
-        <p>Count: {count}</p>
-        <span onClick={() => {displayCard.current = true}}>Display my card</span>
-        {Cardd('Ma super card', 'description')}
+      <h1>Mini React app</h1>
+      <Btn label="Click me" onClick={() => handleClick()} /> 
+      <p>Count: {count}</p>
+      <span onClick={() => {displayCard.current = true}}>Display my card</span>
+      {pokemons.map((pokemon: any) => Card(pokemon.name, pokemon.url))}
     </div>
   )
 }
